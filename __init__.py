a83df67a2a0dcb4b8aed43439e5c15364ed4bbc6
youht88 <1742716458@qq.com>
2025-09-17 09:48:10 +0800

    add audio

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ class PyScript:
     """
     CATEGORY = "youht"
     FUNCTION = "generate"
-    RETURN_TYPES = ("STRING","INT","FLOAT","BOOLEAN", "IMAGE","STRING","STRING")
-    RETURN_NAMES = ("字符串","整数","浮点数","布尔值","图像","status","show_help")
+    RETURN_TYPES = ("STRING","INT","FLOAT","BOOLEAN", "IMAGE","AUDIO","STRING","STRING")
+    RETURN_NAMES = ("字符串","整数","浮点数","布尔值","图像","音频","status","show_help")
     OUTPUT_NODE = False
     @classmethod
     def INPUT_TYPES(cls):
@@ -36,6 +36,7 @@ class PyScript:
         obj_float = None
         obj_boolean = None
         obj_image = None
+        obj_audio = None
         try:
             if type(obj) == str:
                 obj_string = obj
@@ -92,13 +93,20 @@ class PyScript:
                     obj_image = obj
                 if obj_image:
                     obj_string = json.dumps(obj_image.shape,ensure_ascii=False)
+            if type(obj) == dict:
+                if "waveform" in obj and "sample_rate" in obj and type(obj["waveform"]) == torch.Tensor and type(obj["sample_rate"])==int:
+                    obj_audio = obj
+                    obj_int = obj["sample_rate"]
+                    obj_string = json.dumps(obj["waveform"].shape,ensure_ascii=False)
+                else:
+                    obj_string = json.dumps(obj,ensure_ascii=False) 
             if type(obj) == tuple:
                 obj_string = json.dumps(list(obj),ensure_ascii=False)
-            if type(obj) in [list,dict]:
+            if type(obj) == list:
                 obj_string = json.dumps(obj,ensure_ascii=False)
         except:
             pass
-        return obj_string,obj_int,obj_float,obj_boolean,obj_image
+        return obj_string,obj_int,obj_float,obj_boolean,obj_image,obj_audio
     def __decode(self,obj):
         if type(obj) == str:
             try:
@@ -116,10 +124,13 @@ class PyScript:
     ):
         show_help=(
         "1. 脚本中可以使用a、b、c、d四个变量，分别对应输入的四个参数,可以是任意类型\n"
-        "2. 脚本中返回结果需要赋值给RESULT变量，系统自动根据结果类型转换为相应的数据格式(数组、字典统一转换为字符串,如果是图像,则输出shape的字符串和原始图像)\n"
+        "2. 脚本中返回结果需要赋值给RESULT变量，系统自动根据结果类型转换为相应的数据格式(数组、字典统一转换为字符串)\n"
         "3. 脚本中可以使用print函数，用于打印日志。\n"
         "4. 脚本中可以使用import语句，导入python标准库或第三方库。\n"
         "5. 脚本中可以使用任何python语法。\n"
+        "6. 如果RESULT是3维或4维torch.Tensor对象，则被认为是图像。输出shape的字符串和原始图像\n"
+        "7. 如果RESULT变量是一个包含waveform和sample_rate两个键的字典，且waveform为torch.Tensor类型，sample_rate为int类型,则被认为是音频。输出shape的字符串和音频\n"
+        "8. 如果没有结果可能script脚本执行出错了，可以查看stats错误信息。status默认情况下是所有变量值"
         "示例:\n"
         "name=a\n"
         "age=b\n"
@@ -136,12 +147,12 @@ class PyScript:
             }        
             exec(script,globals(),result_dict)
             result = result_dict.get("RESULT", "")
-            result_string,result_int,result_float,result_boolean,result_image = self.__encode(result)
+            result_string,result_int,result_float,result_boolean,result_image,result_audio = self.__encode(result)
             status = f"所有内部变量为{result_dict}"
-            return (result_string,result_int,result_float,result_boolean, result_image, status,show_help)
+            return (result_string,result_int,result_float,result_boolean, result_image, result_audio,status,show_help)
         except Exception as e:
             status = f"执行失败:{e}"
-            return (None,None,None,None,None, status,show_help)
+            return (None,None,None,None,None,None, status,show_help)
 
 # 注册到ComfyUI
 NODE_CLASS_MAPPINGS = {
